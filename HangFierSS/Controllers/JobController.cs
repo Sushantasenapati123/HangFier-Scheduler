70dae9e628f185c685c829b69987c22c23b19453
sushanta.senapati <sushanta.senapati@CSMBHUL954>
2023-08-29 10:50:45 +0530

    ok

---
+++ HangFierSS/Controllers/JobController.cs
@@ -0,0 +1,35 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace HangFierSS.Controllers
+{
+    public class JobController : Controller
+    {
+        private readonly IRecurringJobManager _recurringJobManager;
+        private readonly IBackgroundJobClient _backgroundJobClient;
+        public JobController(IRecurringJobManager recurringJobManager, IBackgroundJobClient backgroundJobClient)
+        {
+            _recurringJobManager = recurringJobManager;
+            _backgroundJobClient = backgroundJobClient;
+        }
+        public IActionResult CreateRecurringJob()
+        {
+            _backgroundJobClient.Enqueue(() => Background());
+            _recurringJobManager.AddOrUpdate("jobId", () => Recurring(), Cron.Minutely);
+            return Ok();
+        }
+        public void Recurring()
+        { // Get the current date and time
+            DateTime currentTime = DateTime.Now;
+            // Log the message with current date and time
+            Log.Information("Recurring job executed at: {CurrentTime}", currentTime);
+
+        }
+
+        public void Background()
+        {
+            Console.WriteLine("Hello HangFire Job!");
+        }
+    }
+}
---
+++ HangFierSS/Program.cs
@@ -0,0 +1,71 @@
+using Hangfire;
+using Hangfire.Logging;
+using Hangfire.SqlServer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Serilog logger configuration
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("wwwroot/logs/HangFier_.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Configuration for Hangfire
+string hangfireConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+builder.Services.AddHangfire(configuration => configuration
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage(hangfireConnectionString, new SqlServerStorageOptions
+    {
+        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+        QueuePollInterval = TimeSpan.Zero,
+        UseRecommendedIsolationLevel = true,
+        DisableGlobalLocks = true
+    }));
+
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+app.UseHangfireDashboard();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Job}/{action=CreateRecurringJob}/{id?}");
+
+app.MapHangfireDashboard();
+
+app.Run();
